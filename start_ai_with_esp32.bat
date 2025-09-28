5bd1e9b0c8b466dced4d15b8552a85d378e25f4c
Mak Moinee <titus.silver.ph@gmail.com>
2025-09-28 22:16:27 +0800

    update code

--- start_ai_with_esp32.bat
+++ start_ai_with_esp32.bat
@@ -1,1 +1,11 @@
-.\Scripts\activate && python api3.py
+@echo off
+REM Activate virtual environment
+call .\Scripts\activate
+
+REM Ask user for camera IP
+set /p CAMERA_IP=Enter ESP32 camera IP (e.g., [http://192.168.1.5:4747](http://192.168.1.5:4747)):
+
+REM Run Python app with the provided IP
+python api3.py --ip %CAMERA_IP%
+
+pause
